3dbcc434a028d10f3a4b94a37e5b21ec4334ce32
jmgindi <jmgindi@gmail.com>
2019-05-17 22:57:51 +0000

    new files and edits for problem 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+const $ = window.$;
+let amenities = [];
+
+$(document).ready(function () {
+    $('.amenity_checkbox').click(function () {
+      let amenity_id = $(this).data('id');
+      if (amenities.includes(amenity_id)) {
+        let index = amenities.indexOf(amenity_id);
+        if (index !== -1) {
+          amenities.splice(index, 1);
+        }
+      } else {
+        amenities.push(amenity_id);
+      }
+      console.log(amenities);
+    });
+  $.get("http://0.0.0.0:5001/api/v1/status", function (data, status) {
+    console.log(data);
+    console.log(status);
+  });
+});
