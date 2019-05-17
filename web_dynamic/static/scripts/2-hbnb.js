2ab15eb6c7faf7de636c4720610b2e0bdd53bd4a
jmgindi <jmgindi@gmail.com>
2019-05-17 23:39:40 +0000

    updated styling to position dot correctly, script to respond

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -15,7 +15,11 @@ $(document).ready(function () {
       console.log(amenities);
     });
   $.get("http://0.0.0.0:5001/api/v1/status", function (data, status) {
-    console.log(data);
-    console.log(status);
+    console.log(data['status'])
+    if (data['status'] === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
   });
 });
